b5f7cf4841956caf2d45e1a4634dfe45c512c7ca
VegaFromLyra <asha.ab@gmail.com>
2014-02-16 18:31:25 -0800

    More binary tree problems + solutions

--- BinaryTreeProblems/Program.cs
+++ BinaryTreeProblems/Program.cs
@@ -5,11 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-   //     8                   8
-   //   /   \               /   \
-   // 7       9   =>       9     7
-   // \                         /
-   //  14                      14
+   //     8 
+   //   /   \
+   // 7       9
+   // \      /
+   //  4    1
 namespace BinaryTreeProblems
 {
     class Program
@@ -19,15 +19,69 @@ namespace BinaryTreeProblems
             Node n = new Node(8);
             n.Left = new Node(7);
             n.Right = new Node(9);
-            n.Left.Right = new Node(14);
+            n.Left.Right = new Node(4);
+            n.Right.Left = new Node(1);
 
             Console.WriteLine("Max node value: {0}", GetMaxNodeValue(n));
 
+            Console.WriteLine("Are leaf nodes at the same level ?: {0}", AreLeafNodesAtSameLevel(n));
+
+            Console.WriteLine("Sum of paths from root: {0}", SumOfPaths(n));
+
+            Console.WriteLine("Max sum represented by binary tree: {0}", MaxSum(n));
+
+            Console.WriteLine("List representation of binary tree");
+
+            LinkedList<int> result = ConvertTreeToDoubleLinkedList(n);
+
+            LinkedListNode<int> node = result.First;
+            while (node != null)
+            {
+                Console.Write(node.Value + " ");
+                node = node.Next;
+            }
+            Console.WriteLine();
+
+            Node deepestLeftNode = GetDeepestLeftNode(n);
+            Console.WriteLine("Deepest left node is {0}", deepestLeftNode.Data);
+
+            Node deepestNodeAtOddLevel = GetNodeAtDeepestOddLevel(n);
+            Console.WriteLine("Node at deepest odd level is {0}", deepestNodeAtOddLevel.Data);
+
+            // Mirror changes the reference of n so keep this as the last test
             Console.WriteLine("In order traversal of mirrored version of n");
             MirrorBinaryTree(n);
             InOrder(n);
 
-            Console.WriteLine("Are leaf nodes at the same level ?: {0}", AreLeafNodesAtSameLevel(n));
+            Node head = new Node(3);
+            head.Left = new Node(1);
+            head.Right = new Node(4);
+
+            Console.WriteLine("Updated version of head");
+            UpdateNode(head);
+            InOrder(head);
+
+            int sum = 21;
+            List<int> output = new List<int>();
+
+            Node test = new Node(7);
+            test.Left = new Node(3);
+            test.Right = new Node(4);
+            test.Right.Right = new Node(1);
+            test.Left.Right = new Node(6);
+            test.Left.Right.Left = new Node(2);
+            test.Left.Right.Right = new Node(5);
+
+            bool result2 = DoesPathAddUp(test, sum, output);
+            Console.WriteLine("Does path add up for sum {0} : {1}", sum, result2);
+
+            if (result2)
+            {
+                for (int i = output.Count - 1; i >= 0; i--)
+                {
+                    Console.Write(output[i] + " ");
+                }
+            }
         }
 
         // Problem 1: Given a binary tree, find node with maximum value
@@ -107,6 +161,207 @@ namespace BinaryTreeProblems
                        AreLeafNodesAtSameLevel(n.Right, currentLevel + 1, ref depth);
             }
         }
+
+        // Problem 5: Given a binary tree with digits (0 - 9), 
+        // return sum of numbers represented by each path
+        // For example, in the above example
+        // answer is 874 + 891
+        static int SumOfPaths(Node n)
+        {
+            int number = 0;
+
+            return Sum(n, number);
+        }
+
+        static int Sum(Node n, int number)
+        {
+           if (n == null)
+           {
+              return 0;
+           }
+
+           // NOTE: This can be modified to support nodes
+           // having values with more than 1 digit
+           number = number * 10 + n.Data;
+
+           if (n.Left == null && n.Right == null)
+           {
+              return number;
+           }
+   
+           return Sum(n.Left, number) + Sum(n.Right, number);
+        }
+
+        // Problem 6: Return max sum represented by any path
+        static int MaxSum(Node n)
+        {
+            if (n == null)
+            {
+                return Int32.MinValue;
+            }
+
+            return Math.Max(n.Data, n.Data + Math.Max(MaxSum(n.Left), MaxSum(n.Right)));
+        }
+
+        // Problem 7: Convert binary tree to double linked list 
+        // For given tree, we could do inorder traversal and create a double linked list
+        // 7 - 4 - 8 - 1 - 9
+        static LinkedList<int> ConvertTreeToDoubleLinkedList(Node n)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+
+            ConvertTreeToDoubleLinkedList(n, list);
+
+            return list;
+        }
+
+        static void ConvertTreeToDoubleLinkedList(Node n, LinkedList<int> list)
+        {
+            if (n == null)
+            {
+                return;
+            }
+
+            ConvertTreeToDoubleLinkedList(n.Left, list);
+
+            list.AddLast(n.Data);
+
+            ConvertTreeToDoubleLinkedList(n.Right, list);
+        }
+
+        // Problem 8: Get deepest left node
+        static Node GetDeepestLeftNode(Node n)
+        {
+            int currentLevel = 1;
+            int maxDepth = 1;
+
+            return GetDeepestLeftNode(n, currentLevel, ref maxDepth, false);
+        }
+
+        static Node GetDeepestLeftNode(Node n, int currentLevel, ref int maxDepth, bool isLeft)
+        {
+           if (n == null)
+           {
+              return null;
+           }
+           else if (n.Left == null && n.Right == null)
+           {
+               if ((currentLevel > maxDepth) && isLeft)
+               {
+                   maxDepth = currentLevel;
+               }
+
+               if (isLeft && currentLevel  == maxDepth)
+               {
+                   return n;
+               }
+
+               return null;
+           }
+           else
+           {
+               Node leftResult = GetDeepestLeftNode(n.Left, currentLevel + 1, ref maxDepth, true);
+               Node rightResult = GetDeepestLeftNode(n.Right, currentLevel + 1, ref maxDepth, false);
+
+               if (leftResult != null)
+               {
+                   return leftResult;
+               }
+               else if (rightResult != null)
+               {
+                   return rightResult;
+               }
+               else
+               {
+                   return null;
+               }
+           }
+        }
+
+
+        // Problem 9: Get node at deepest odd level
+        // If there is more than 1 node at the same level
+        // which happens to be the deeepest odd level then
+        // return the first one that is encountered
+        static Node GetNodeAtDeepestOddLevel(Node n)
+        {
+            int currentLevel = 1;
+            int maxOddDepth = 1;
+
+            return GetNodeAtDeepestOddLevel(n, currentLevel, ref maxOddDepth);
+        }
+
+        static Node GetNodeAtDeepestOddLevel(Node n, int currentLevel, ref int maxOddDepth)
+        {
+            if (n == null)
+            {
+                return null;
+            }
+            else if (n.Left == null && n.Right == null)
+            {
+               if ((currentLevel % 2 != 0) && (currentLevel > maxOddDepth))
+               {
+                  maxOddDepth = currentLevel;
+                  return n;
+               }
+
+               return null;
+            }
+            else
+            {
+                Node leftResult = GetNodeAtDeepestOddLevel(n.Left, currentLevel + 1, ref maxOddDepth);
+                Node rightResult = GetNodeAtDeepestOddLevel(n.Right, currentLevel + 1, ref maxOddDepth);
+
+                return leftResult != null ? leftResult : rightResult;
+            }
+        }
+
+        // Problem 11: Given a binary search tree, update each node to contain the sum
+        // of its key and all keys greater. Example below
+
+         //     8                         13
+         //   /   \           =>         /   \
+         //  2     5                  15       5
+        static void UpdateNode(Node n)
+        {
+            int value = 0;
+            UpdateNode(n, ref value);
+        }
+
+        static void UpdateNode(Node n, ref int value)
+        {
+            if (n == null)
+            {
+                return;
+            }
+
+            UpdateNode(n.Right, ref value);
+
+            n.Data += value;
+            value = n.Data;
+
+            UpdateNode(n.Left, ref value);
+        }
+
+         // Problem 12 : Given a value, determine if any path of the given binary tree
+         // adds up to the value and if so print the path
+        static bool DoesPathAddUp(Node n, int sum, List<int> output)
+        {
+            if (n == null)
+            {
+                return sum == 0;
+            }
+
+            bool leftResult = DoesPathAddUp(n.Left, sum - n.Data, output);
+            bool rightResult = DoesPathAddUp(n.Right, sum - n.Data, output);
+
+            if (leftResult || rightResult)
+            {
+                output.Add(n.Data);
+            }
+
+            return leftResult || rightResult;
+        }
     }
 
     
@@ -118,7 +373,7 @@ namespace BinaryTreeProblems
             Data = data;
         }
 
-        public int Data { get; private set; }
+        public int Data { get; set; }
 
         public Node Left { get; set; }
 
